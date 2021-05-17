21880ae3b31ac3cb5c8167b4c479eafd5d8e3f05
Ruslan Rozumnyak <rusroz77@gmail.com>
2021-05-17 13:30:59 +0300

    testForm

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -5,6 +5,7 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddlware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 let redusers = combineReducers({
    profilePage: profileReducer,
@@ -12,6 +13,7 @@ let redusers = combineReducers({
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     authReducer: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(redusers, applyMiddleware(thunkMiddlware));
